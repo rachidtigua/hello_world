9f32119adb3f0fa6c92b1b8bdf4342c1f594504c
Rachid TIGUA <tigua.rachid@gmail.com>
2024-01-14 12:42:09 +0100

    Ajout des fonctions BornerS16 et BornerU16

--- Lib/Util.c
+++ Lib/Util.c
@@ -7,3 +7,19 @@ void BornerByte(BYTE *val, BYTE max, BYTE min)
 	if (*val < min)
 		*val = min;
 }
+
+void BornerS16(S16 *val, S16 max, S16 min)
+{
+	if (*val > max)
+		*val = max;
+	if (*val < min)
+		*val = min;
+}
+
+void BornerU16(U16 *val, U16 max, U16 min)
+{
+	if (*val > max)
+		*val = max;
+	if (*val < min)
+		*val = min;
+}
--- Lib/Util.h
+++ Lib/Util.h
@@ -1,11 +1,15 @@
 #ifndef UTIL_H_
 #define UTIL_H_
 
-#define BYTE unsigned char
+#define BYTE 	unsigned char
+#define U16	unsigned short
+#define S16	short
 
 #define MAX(x,y) 	((x) < (y) ? (y) : (x))
 #define MIN(x,y)	((x) > (y) ? (y) : (x))
 
 void BornerByte(BYTE *val, BYTE valMax, BYTE valMin);
+void BornerS16(S16 *val, S16 max, S16 min);
+void BornerU16(U16 *val, U16 max, U16 min);
 
 #endif
